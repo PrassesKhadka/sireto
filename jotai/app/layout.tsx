02658931c86f5207f7d45a6351c0a55aafff8506
Prasses Khadka <khadkaprasses@gmail.com>
2024-03-12 09:58:59 +0545

    .

--- jotai/app/layout.tsx
+++ jotai/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {Provider} from "jotai"
+import { Provider } from "jotai";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Provider>
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>{children}</body>
       </Provider>
     </html>
   );
